92d97b2d86e63ea1b69ca3a2cbfde0f050d3f6ca
NurKeinNeid <mralexman3000@gmail.com>
2021-05-29 12:56:31 +0530

    guacamole: Remove some packages
    Change-Id: I1f318ae8f0263177040e10baa25952c1accdea32
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/first_stage_ramdisk/fstab.qcom
 
+# Remove some prebuilt apps
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
