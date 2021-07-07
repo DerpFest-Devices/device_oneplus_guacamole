bf559dbdc78db422224c01bed82c37d5b39bf331
Pranav <npv12@iitbbs.ac.in>
2021-07-07 22:43:46 +0530

    guacamole: prebuilts: Drop cameraRoll as well
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackagesDevice
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Drive CalendarGooglePrebuilt Chrome Chrome-Stub Maps Photos PrebuiltGmail PixelWallpapers2020 RecorderPrebuilt SafetyHubPrebuilt FilesPrebuilt Ornament PixelLiveWallpaperPrebuilt
+LOCAL_OVERRIDES_PACKAGES := Drive CalendarGooglePrebuilt Chrome Chrome-Stub Maps Photos PrebuiltGmail PixelWallpapers2020 RecorderPrebuilt SafetyHubPrebuilt FilesPrebuilt Ornament PixelLiveWallpaperPrebuilt CameraRoll
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
